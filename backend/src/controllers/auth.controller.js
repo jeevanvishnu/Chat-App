3cb765993c70f2fd5b43b196bcd3d8bc6ead5da8
Jeevan Vishnu <140521914+jeevanvishnu@users.noreply.github.com>
2025-09-11 13:50:57 +0530

    Add bcrypt for password hashing in signup flow
    Introduced bcrypt as a dependency and updated the signup controller to hash user passwords before saving. Also improved input validation and error handling in the signup process. Updated server.js to include body parsing middleware for JSON and URL-encoded data.

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,36 @@
-export const signup = (req,res) =>{
- 
+
+import status from 'http-status'
+import User from '../model/user.model.js'
+import bcrypt from 'bcrypt'
+
+export const signup = async (req,res) =>{
+  const {firstName , email , password} = req.body
     try {
+
+        if(password.length < 6) {
+          return   res.status(status[400]).json({message:"Password must be atleast 6 letter"})
+        }
         
+        const user = User.findOne({email:email})
+
+        if(user) {
+            return res.status(status[400]).json({message:'Email is already exists'})
+        }
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt)
+
+        const newUser = new User({
+            fullName,
+            email,
+            password:hashedPassword
+        })
+
+        if(newUser){
+            
+        }else{
+            res.status(status[400]).json({message:'Invaild user data'})
+        }
+
     } catch (error) {
         
     }
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
-import logger from 'morgan'
+import logger from "morgan";
 import authRouter from "./router/auth.router.js";
 import { connectDb } from "./lib/db.js";
+
 dotenv.config();
 const app = express();
 
 // middleware setup
-app.use(logger('dev'))
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", authRouter);
 
 // Port setup
 const PORT = process.env.PORT || 3000;
-connectDb()
-.then(()=>{
-   app.listen(PORT,()=>{
+connectDb().then(() => {
+  app.listen(PORT, () => {
     console.log(`The Port has running on ${PORT}`);
-    
-   })
-})
+  });
+});
