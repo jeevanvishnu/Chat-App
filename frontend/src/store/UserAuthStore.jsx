1957775e38bba2acaf2c5f6c465faa5750388e9a
Jeevan Vishnu <140521914+jeevanvishnu@users.noreply.github.com>
2025-09-16 23:01:19 +0530

    Add auth check and loader to frontend, enable CORS in backend
    Implemented authentication check in the frontend using Zustand store and added a PageLoader component for loading states. Updated routing to redirect based on authentication status. Integrated lucide-react for loader icon. Fixed axios baseURL logic and enabled CORS in the backend to allow credentials from the client URL.

--- frontend/src/store/UserAuthStore.jsx
+++ frontend/src/store/UserAuthStore.jsx
@@ -1,7 +1,20 @@
 import {create} from 'zustand'
-
+import {axiosInstance} from "../lib/axios"
 
 export const userAuthStore = create((set)=>({
-    authUser:{name:"Jeevan" , _id:1 , age:20},
-    loading:false
+    authUser:null,
+    isCheckingAuth:true,
+
+    checkAuth : async ()=>{
+        try {
+            const res =  await axiosInstance.get('/api/auth/check')
+            set({authUser:res.data})
+        } catch (error) {
+            console.log("Error in authUser" , error);
+            set({authUser:null})
+            
+        }finally{
+            set({isCheckingAuth:false})
+        }
+    }
 }))
